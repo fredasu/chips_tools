0ebc027c531791d1cff0d5c009b89e67adf5ab8c
fredasu <feisu9@gmail.com>
2024-02-11 22:01:51 -0500

    Loading DAT files

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -2,7 +2,13 @@ import cc_dat_utils
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
-
+outFile = "data/pfgd_test.txt"
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+data = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
 #print the resulting data
+print(data) #I feel like this line is redundant if we're writing directly to a text file anyways
+dataString = data.__str__() #for writing compatability
+#write to a file in the pipeline because copy pasting is annoying
+with open(outFile, 'w') as writer: # based on "write_cc_level_pack_to_dat" fxn
+    writer.write(dataString)
